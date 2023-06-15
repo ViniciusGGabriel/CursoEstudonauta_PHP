3dfe83826658b0fbfa044604f37a85f3840bfbfe
Vinícius Gabriel <viniciusggabrielpl@gmail.com>
2023-06-15 20:27:59 -0300

    Primeiros exercícios
    Primeiro código e exercício do curso

---
+++ ex000/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro código em PHP</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Ola mundo \u{1f341}";
+            print " Segundo ola mundo"
+            /* Funciona nas duas formas */
+        ?>
+    </h1>
+    <p>Botando fogo na maldição </p>
+</body>
+</html>
