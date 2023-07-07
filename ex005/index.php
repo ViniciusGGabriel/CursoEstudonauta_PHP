4e0e9218f3b4f1daeefcbd48b87de2cbef81bc47
Vinícius Gabriel <viniciusggabrielpl@gmail.com>
2023-07-07 10:37:26 -0300

    ex005
    Testando a diferença de operadores no php

---
+++ ex005/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testando operadores</title>
+</head>
+<body>
+    <?php 
+     $n = "2" + "2";
+     $n1 = 2 + 2;
+
+     $s = "2" . "2";
+    ?>
+</body>
+</html>
